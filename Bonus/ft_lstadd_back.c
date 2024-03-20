6281bc6afa4fc40427b34b3bdcc02757ba1d55cd
“rolee” <rolee@student.42seoul.kr>
2024-03-20 17:39:43 +0900

    complete

---
+++ Bonus/ft_lstadd_back.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rolee <rolee@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 15:57:13 by rolee             #+#    #+#             */
+/*   Updated: 2024/03/20 17:38:26 by rolee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *last_node = ft_lstlast(*lst);
+	if (last_node == NULL)
+		*lst = new;
+	else
+		last_node->next = new;
+}

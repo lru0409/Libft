7abb228747eeb2f94145626c0c558790ed1de29b
“rolee” <rolee@student.42seoul.kr>
2024-03-22 16:27:53 +0900

    solve norm error

--- Mandatory/Part2/ft_strjoin.c
+++ Mandatory/Part2/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rolee <rolee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 16:55:46 by rolee             #+#    #+#             */
-/*   Updated: 2024/03/20 17:08:17 by rolee            ###   ########.fr       */
+/*   Updated: 2024/03/22 16:27:32 by rolee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-
 	total_len = ft_strlen(s1) + ft_strlen(s2);
 	new_str = (char *)malloc(total_len + 1);
 	if (!new_str)
 		return (NULL);
-
 	new_str[0] = 0;
 	new_str = ft_strcat(new_str, s1);
 	new_str = ft_strcat(new_str, s2);
